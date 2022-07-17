12979eec6367eb06dcf8d214c82073883af2d351
ella-chetvertak <olegcetvertak037@gmail.com>
2022-07-17 17:43:23 +0300

    first page

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// let text = document.createElement("p")
+// let label1 = document.getElementById("label1")
+// let label = document.getElementById("label")
+// document.forms[0].addEventListener("submit", function(event) {
+//     event.preventDefault()
+//     if (this.quest.value === "2") {
+//         document.body.appendChild(text)
+//         text.innerText = "Ura pobeda"
+//         label1.style.display = "block"
+//         label.style.display = "none"
+//         if (this.quest.value === "2") {}
+//     } else if (parseInt(this.quest.value)) {
+//         document.body.appendChild(text)
+//         text.innerText = "Неправильно"
+//     } else {
+//         document.body.appendChild(text)
+//         text.innerText = "Введи число"
+//     }
+// })
+
+{/* <form name="genius">
+    <label for="quest" id="label">Сколько сейчас времени если сейчас два часа дня?</label>
+    <label for="quest" id="label1">2 + 2 =</label>
+    <input name="quest" type="text" class="input1" placeholder="ответ подавай">
+</form> */}
+
+//==========================================================================
+
